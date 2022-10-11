e824c64f1523e6fa98072215eec178796cf8a06b
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-10-11 16:14:15 +0100

    Add attribute hauteur which has the nomber of each card

--- src/com/game/Point.java
+++ src/com/game/Point.java
@@ -1,23 +1,25 @@
 package com.game;
 
 public enum Point {
-    ACE_MIN(1),
-    ACE_MAX(11),
-    DEUX(2),
-    TROIS(3),
-    QUATRE(4),
-    CINQ(5),
-    SIX(6),
-    SEPT(7),
-    HUIT(8),
-    NEUF(9),
-    DIX(10),
-    VALET(11),
-    DAME(12),
-    ROI(13);
-    int point;
+    ACE_MIN(1,1),
+    ACE_MAX(11,1),
+    DEUX(2,2),
+    TROIS(3,3),
+    QUATRE(4,4),
+    CINQ(5,5),
+    SIX(6,6),
+    SEPT(7,7),
+    HUIT(8,8),
+    NEUF(9,9),
+    DIX(10,10),
+    VALET(10,11),
+    DAME(10,12),
+    ROI(10,13);
+    final int point;
+    final int hauteur;
 
-    Point(int point) {
+    Point(int point,int hauteur) {
         this.point = point;
+        this.hauteur=hauteur;
     }
 }
