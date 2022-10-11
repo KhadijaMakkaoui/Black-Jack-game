3f2936136202dc0d5e908527262c1cb36e6648af
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-10-11 15:15:06 +0100

    create Enum SuiteCouleur that contains the four suits with their color, nomber of cards and name

---
+++ src/com/game/SuiteCouleur.java
@@ -0,0 +1,26 @@
+package com.game;
+
+public enum SuiteCouleur {
+    CARREAUX(13,"Rouge","Carreaux"),
+    COEUR(13,"Rouge","Coeur"),
+    PIQUE(13,"Noir","Pique"),
+    TREFLE(13,"Noir","Carreaux");
+    final int nombre_carte;
+    final String nom;
+    String Couleur;
+
+    SuiteCouleur(int nombre_carte, String nom, String couleur) {
+        this.nombre_carte = nombre_carte;
+        this.nom = nom;
+        Couleur = couleur;
+    }
+
+    @Override
+    public String toString() {
+        return "SuiteCouleur{" +
+                "nombre_carte=" + nombre_carte +
+                ", nom='" + nom + '\'' +
+                ", Couleur='" + Couleur + '\'' +
+                '}';
+    }
+}
