9cdbe62d493822815185d15719c01e62a2ee2f31
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-10-12 09:38:56 +0100

    Add name attribute to enum Point

--- src/com/game/Jeu.java
+++ src/com/game/Jeu.java
@@ -2,6 +2,7 @@ package com.game;
 
 public class Jeu {
     public static void main(String[] args) {
+        System.out.println("-------------------Welcome to Blackjack---------------------");
 
     }
 }
--- src/com/game/Point.java
+++ src/com/game/Point.java
@@ -1,25 +1,34 @@
 package com.game;
 
 public enum Point {
-    ACE_MIN(1,1),
-    ACE_MAX(11,1),
-    DEUX(2,2),
-    TROIS(3,3),
-    QUATRE(4,4),
-    CINQ(5,5),
-    SIX(6,6),
-    SEPT(7,7),
-    HUIT(8,8),
-    NEUF(9,9),
-    DIX(10,10),
-    VALET(10,11),
-    DAME(10,12),
-    ROI(10,13);
+    ACE_MIN("Ace",1,1),
+    ACE_MAX("Ace",11,1),
+    DEUX("Deux",2,2),
+    TROIS("Trois",3,3),
+    QUATRE("Quatre",4,4),
+    CINQ("Cinq",5,5),
+    SIX("Six",6,6),
+    SEPT("Sept",7,7),
+    HUIT("Huit",8,8),
+    NEUF("Neuf",9,9),
+    DIX("Dix",10,10),
+    VALET("Valet",10,11),
+    DAME("Dame",10,12),
+    ROI("Roi",10,13);
+    final String nom;
     final int point;
     final int hauteur;
 
-    Point(int point,int hauteur) {
+    Point(String nom,int point,int hauteur) {
+        this.nom=nom;
         this.point = point;
         this.hauteur=hauteur;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "nom='" + nom + '\'' +
+                '}';
     }
 }
