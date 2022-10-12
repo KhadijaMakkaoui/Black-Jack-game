250c965a321d39effd2686108060153cb693048e
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-10-12 10:55:55 +0100

    Create attributes and conctructor in Pioche class and create ajouterCarte() to add a card inside the arrayList of cardsÃ

--- src/com/game/Jeu.java
+++ src/com/game/Jeu.java
@@ -3,7 +3,11 @@ package com.game;
 public class Jeu {
     public static void main(String[] args) {
         System.out.println("-------------------Welcome to Blackjack---------------------");
-        Carte c=new Carte(SuiteCouleur.TREFLE,Point.DIX);
-        System.out.println(c.toString());
+        Carte c1=new Carte(SuiteCouleur.TREFLE,Point.DIX);
+        Carte c2=new Carte(SuiteCouleur.CARREAUX,Point.ACE_MIN);
+        Pioche p=new Pioche();
+        p.ajouterCarte(c1);
+        p.ajouterCarte(c2);
+        System.out.println(p.toString());
     }
 }
--- src/com/game/Pioche.java
+++ src/com/game/Pioche.java
@@ -1,4 +1,19 @@
 package com.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pioche {
+    Carte carte;
+    private List<Carte> pioche=new ArrayList<>();
+    public void ajouterCarte(Carte carte){
+        pioche.add(carte);
+    }
+
+    @Override
+    public String toString() {
+        return "Pioche{" +
+                "\npioche=" + pioche +
+                "}";
+    }
 }
