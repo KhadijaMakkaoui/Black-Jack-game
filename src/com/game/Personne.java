d48d067efb5c6a36983fddbe01c9591da63b5afe
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-10-17 13:31:40 +0100

    Create getters and setters

---
+++ src/com/game/Personne.java
@@ -0,0 +1,22 @@
+package com.game;
+
+public class Personne {
+    private String nom;
+    private Hand hand;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+}
